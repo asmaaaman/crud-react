cdb7a63f25e494208a1556a94d164001371e6aa9
asmaaaman <asmaa.aman91@gmail.com>
2023-11-24 16:47:23 +0200

    fix:footer ui issue

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <div
-        className="fixed bottom-0 left-0 right-0 z-40 px-4 py-3 border-t	
+        className="relative mt-40 bottom-0 left-0 right-0 z-40 px-4 py-3 border-t	
         border-indigo shadow-md mx-auto p-3 md:p-6 text-center
         text-gray-800"
       >
